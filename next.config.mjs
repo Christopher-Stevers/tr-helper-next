8aeaca87d96d578917b777cafa27736b4683db6e
Christopher-Stevers <christopher.stevers@protonmail.com>
2024-10-22 12:08:22 -0400

    use correct helper

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.API_URL || "http://64.227.3.191:3000",
+    NEXT_PUBLIC_API_URL: process.env.API_URL || "httpstr-helper.com",
   },
 };
 
