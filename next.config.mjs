fad29d4de5a14ebcbf547534349d70e2996f9eef
Christopher-Stevers <christopher.stevers@protonmail.com>
2024-10-22 12:14:11 -0400

    use tr-helper

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.API_URL || "httpstr-helper.com",
+    NEXT_PUBLIC_API_URL: "https://tr-helper.com",
   },
 };
 
